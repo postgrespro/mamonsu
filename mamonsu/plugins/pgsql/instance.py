f1b4f9ca1eb423978e5313b4535e2e8b2b5ae51f
Dmitry Vasilyev <d.vasilyev@postgrespro.ru>
2016-02-10 14:10:21 +0300

    returned на другой стороне графика

--- mamonsu/plugins/pgsql/instance.py
+++ mamonsu/plugins/pgsql/instance.py
@@ -33,11 +33,11 @@ class Instance(Plugin):
         ('tup_deleted', 'tuples[deleted]', 'tuples: deleted',
             ('PostgreSQL instance: tuples', '000000', 0), None, 1),
         ('tup_fetched', 'tuples[fetched]', 'tuples: fetched',
-            ('PostgreSQL instance: tuples', '0000CC', 1), None, 1),
+            ('PostgreSQL instance: tuples', '0000CC', 0), None, 1),
         ('tup_inserted', 'tuples[inserted]', 'tuples: inserted',
             ('PostgreSQL instance: tuples', '00CC00', 0), None, 1),
         ('tup_returned', 'tuples[returned]', 'tuples: returned',
-            ('PostgreSQL instance: tuples', 'CC00CC', 0), None, 1),
+            ('PostgreSQL instance: tuples', 'CC00CC', 1), None, 1),
         ('tup_updated', 'tuples[updated]', 'tuples: updated',
             ('PostgreSQL instance: tuples', 'CC0000', 0), None, 1),
     ]
