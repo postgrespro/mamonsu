9a89a96368bd491b6ecd4f83e0fb6191bd7137a7
Dmitry Vasilyev <vadv.mkn@gmail.com>
2016-08-26 11:09:05 +0300

    test improve

--- tests/check.sh
+++ tests/check.sh
@@ -1,32 +1,81 @@
 #!/bin/sh -ex
 
+export TESTDIR=$(dirname $0)
+export ZABBIX_CLIENT_HOST=zabbix_client_host
+export ZABBIX_TEMPLATE=/tmp/template.xml
+export ZABBIX_TEMPLATE_NAME='PostgresPro'
+
 # install mamonsu
 cp -a /var/tmp /root/mamonsu && pushd /root/mamonsu
 yum install -y tar make rpm-build python2-devel python-setuptools
-make rpm && rpm -i mamonsu*.rpm
+make rpm && yum install -y mamonsu*.rpm
 
 # install postgres
 yum install -y https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm
 yum install -y postgresql95-server
 su postgres -c '/usr/pgsql-9.5/bin/initdb -D /var/lib/pgsql/9.5/data'
 /etc/init.d/postgresql-9.5 start
-sleep 5
 
 # mamonsu report
 (mamonsu report | grep version | grep 'PostgreSQL 9.5') || exit 1
 
-# export zabbix template
-mamonsu -e /tmp/template.xml
+# export config
+mamonsu -w /dev/null
+
+# write zabbix template
+mamonsu -e $ZABBIX_TEMPLATE -t $ZABBIX_TEMPLATE_NAME
 grep 'pgsql\.uptime\[\]' /tmp/template.xml || exit 2
 grep 'system\.disk\.all_read' /tmp/template.xml || exit 2
 
-# export config
-mamonsu -w /tmp/agent.conf
+# install zabbix
+yum install -y http://repo.zabbix.com/zabbix/2.4/rhel/6/x86_64/zabbix-release-2.4-1.el6.noarch.rpm
+yum install -y zabbix-server-pgsql zabbix-web-pgsql
+sed -i "s,;date.timezone =,date.timezone = 'US/Eastern'," /etc/php.ini
+su postgres -c 'createdb zabbix'
+su postgres -c 'createuser -a -d -E zabbix'
+psql -1 -f $TESTDIR/zabbix_sql/schema.sql -U zabbix -d zabbix
+psql -1 -f $TESTDIR/zabbix_sql/images.sql -U zabbix -d zabbix
+psql -1 -f $TESTDIR/zabbix_sql/data.sql -U zabbix -d zabbix
+cat <<EOF > /etc/zabbix/zabbix_server.conf
+AlertScriptsPath=/usr/lib/zabbix/alertscripts
+ExternalScripts=/usr/lib/zabbix/externalscripts
+LogFile=/var/log/zabbix/zabbix_server.log
+LogFileSize=0
+PidFile=/var/run/zabbix/zabbix_server.pid
+SNMPTrapperFile=/var/log/snmptt/snmptt.log
+
+DBHost=127.0.0.1
+DBName=zabbix
+DBUser=zabbix
+EOF
+cat <<EOF > /usr/share/zabbix/conf/zabbix.conf.php
+<?php
+global \$DB;
+\$DB["TYPE"]      = 'POSTGRESQL';
+\$DB["SERVER"]    = 'localhost';
+\$DB["PORT"]      = '5432';
+\$DB["DATABASE"]  = 'zabbix';
+\$DB["USER"]      = 'zabbix';
+\$DB["SCHEMA"]    = '';
+\$ZBX_SERVER      = 'localhost';
+\$ZBX_SERVER_PORT = '10051';
+\$ZBX_SERVER_NAME = '';
+\$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+?>
+EOF
+cp /usr/share/zabbix/conf/zabbix.conf.php /etc/zabbix/web/zabbix.conf.php
+/etc/init.d/zabbix-server start
+/etc/init.d/httpd start
+
+# export template to zabbix
+python2 $TESTDIR/export_template.py
 
 # start mamonsu and sleep
 cat <<EOF > /etc/mamonsu/agent.conf
 [zabbix]
-enabled = False
+enabled = True
+address = 127.0.0.1
+client = $ZABBIX_CLIENT_HOST
 
 [metric_log]
 enabled = True
