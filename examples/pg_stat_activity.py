4e1211dc850e850440937a53b57ecfce1a50e980
sergey grinko <grufos@mail.ru>
2016-09-22 15:22:45 +0300

    Update pg_stat_activity.py
    Теперь используется только pg_stat_statements и все ENUM определения, а также учтена сортировка при выдаче items

--- examples/pg_stat_activity.py
+++ examples/pg_stat_activity.py
@@ -1,122 +1,107 @@
 # -*- coding: utf-8 -*-
 
-from mamonsu.plugins.pgsql.plugin import PgsqlPlugin as Plugin
+from mamonsu.lib.plugin import Plugin
 from mamonsu.plugins.pgsql.pool import Pooler
 
 
 # Считаем нагрузку по количеству выполняемых запросов в БД
-class Activity(Plugin):
+class SparkmesActivity(Plugin):
     # execute method run() every 60s
     Interval = 60
-    Yprev = [
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    Coef = 0.5
-    # key, zbx_key, description, units, delta, field_position
-    #   0      1         2         3      4          5
+    Coef = 0.4
+    # key, zbx_key, description, units, delta, field_position  value_type
+    #   0      1         2         3      4          5             6
     Items = [
-        ('calls_upd', 'pgsql.queries.test',
-            'число запросов (modify)', 'n/m', Plugin.DELTA.simple_change, 1),
-        ('calls_sel', 'pgsql.queries.test',
-            'число запросов (select)', 'n/m', Plugin.DELTA.simple_change, 1),
+        ('calls_upd', 'pgsql.queries.sparkmes', 'число запросов (modify)', 'n/m', Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
+        ('calls_sel', 'pgsql.queries.sparkmes', 'число запросов (select)', 'n/m', Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
 
-        ('time_upd', 'pgsql.queries.test',
-            'время (modify)', 'ms', Plugin.DELTA.simple_change, 2),
-        ('time_sel', 'pgsql.queries.test',
-            'время (select)', 'ms', Plugin.DELTA.simple_change, 2),
+        ('time_upd', 'pgsql.queries.sparkmes', 'время (modify)', Plugin.UNITS.ms, Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
+        ('time_sel', 'pgsql.queries.sparkmes', 'время (select)', Plugin.UNITS.ms, Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
 
-        ('callsf', 'pgsql.queries.test',
-            'число запросов (f)', 'n/m', Plugin.DELTA.as_is, 1),
-        ('callsi_s', 'pgsql.queries.test',
-            'число запросов (i)',   'n/m', Plugin.DELTA.as_is, 1),
+        ('callsfirm', 'pgsql.queries.sparkmes', 'число запросов (firm)', 'n/m', Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
+        ('callsip_s', 'pgsql.queries.sparkmes', 'число запросов (ip)',   'n/m', Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
 
-        ('timef', 'pgsql.queries.test',
-            'время (f)', 'ms', Plugin.DELTA.as_is, 2),
-        ('timei_s', 'pgsql.queries.test',
-            'время (i)',   'ms', Plugin.DELTA.as_is, 2),
+        ('timefirm', 'pgsql.queries.sparkmes', 'время (firm)', Plugin.UNITS.ms, Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
+        ('timeip_s', 'pgsql.queries.sparkmes', 'время (ip)',   Plugin.UNITS.ms, Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
     ]
     # name, type, ('graph item', color, yaxisside, drawtype)
     #   0     1         2.0        2.1     2.2       2.3
     Graphs = [
-        ('test активность (calls)', 0, (
-            ('calls_upd',       'CCFFCC', 1, 5),
-            ('calls_sel',       'CCFFFF', 1, 5),
-            ('calls_upd_Yprev', '009900', 1, 2),
-            ('calls_sel_Yprev', '009999', 1, 2),),),
-        ('test активность (time)', 0,  (
-            ('time_upd',        'CCFFCC', 1, 5),
-            ('time_sel',        'CCFFFF', 1, 5),
-            ('time_upd_Yprev',  '009900', 1, 2),
-            ('time_sel_Yprev',  '009999', 1, 2),),),
-        ('test поиск (calls)', 0, (
-            ('callsf',       'CCFFCC', 1, 5),
-            ('callsi_s',       'CCFFFF', 0, 5),
-            ('callsf_Yprev', '009900', 1, 2),
-            ('callsi_s_Yprev', '009999', 0, 2),),),
-        ('test поиск (time)', 0, (
-            ('timef',         'CCFFCC', 1, 5),
-            ('timei_s',         'CCFFFF', 0, 5),
-            ('timef_Yprev',   '009900', 1, 2),
-            ('timei_s_Yprev',   '009999', 0, 2),),),
+        ('sparkmes активность (calls)', 0, (('calls_upd',       'CCFFCC', 1, 5),
+                                            ('calls_sel',       'CCFFFF', 1, 5),
+                                            ('calls_upd_Yprev', '009900', 1, 2),
+                                            ('calls_sel_Yprev', '009999', 1, 2),),),
+        ('sparkmes активность (time)', 0,  (('time_upd',        'CCFFCC', 1, 5),
+                                            ('time_sel',        'CCFFFF', 1, 5),
+                                            ('time_upd_Yprev',  '009900', 1, 2),
+                                            ('time_sel_Yprev',  '009999', 1, 2),),),
+        ('sparkmes поиск (calls)', 0, (('callsfirm',       'CCFFCC', 1, 5),
+                                       ('callsip_s',       'CCFFFF', 0, 5),
+                                       ('callsfirm_Yprev', '009900', 1, 2),
+                                       ('callsip_s_Yprev', '009999', 0, 2),),),
+        ('sparkmes поиск (time)', 0, (('timefirm',         'CCFFCC', 1, 5),
+                                      ('timeip_s',         'CCFFFF', 0, 5),
+                                      ('timefirm_Yprev',   '009900', 1, 2),
+                                      ('timeip_s_Yprev',   '009999', 0, 2),),),
     ]
+    Yprev = [None] * len(Items)
 
     def run(self, zbx):
         # execute query on default database
         result = Pooler.query("""
-
         SELECT '_upd' as type,
                sum(calls) as calls,
                sum(total_time) as totaltime
         FROM pg_stat_statements
-        where dbid = (
-            select datid from pg_stat_database where datname = 'test')
+        where dbid = (select datid from pg_stat_database where datname = 'sparkmes')
           and left(query,8) = 'SELECT *' and query ~* '_UPDATE'
-
         union all
-
         SELECT '_sel' as type,
                sum(calls) as calls,
                sum(total_time) as totaltime
         FROM pg_stat_statements
-        where dbid = (
-            select datid from pg_stat_database where datname = 'test')
+        where dbid = (select datid from pg_stat_database where datname = 'sparkmes')
           and left(query,8) = 'SELECT *' and not (query ~* '_UPDATE')
-
         union all
-
-        select left(
-                substring(
-                    function_name, strpos(function_name,'.')+1, 64),4) as type,
-               count(*) as calls,
-               max(extract(milliseconds from duration)) as time
-        from statement_log -- таблица со статистикой
-        where dt_start >= now()-'00:01:00'::interval
-        group by left(
-            substring(
-                function_name, strpos(function_name,'.')+1, 64),4)
-        order by 1
-
-        """, 'test')
+		select substring(query,15,4) as type, sum(calls) as calls, sum(total_time) as totaltime
+		FROM pg_stat_statements 
+		where dbid = (select datid from pg_stat_database where datname = 'sparkmes') 
+		  and (left(query,27) = 'SELECT * FROM firms_select('
+		       OR
+		       left(query,26) = 'SELECT * FROM ip_select((('
+		      )
+		group by 1
+        """, 'sparkmes')
         # send a resulting value to zabbix
         for idx, item in enumerate(self.Items):
-            key, zbxkey, val = item[0], item[1], 0
+            key, zbxkey, val, delta = item[0], item[1], 0, item[4]
+            #self.log.info('{0}[{1}] '.format(zbxkey, key)+str(idx))
             for row in result:
                 if key.endswith(row[0]):
                     val = row[item[5]]
-                    self.Yprev[idx] = round(
-                        self.Coef * float(val) + (
-                            1 - self.Coef) * self.Yprev[idx], 1)
+                    if self.Yprev[idx] is None:
+                        self.Yprev[idx] = float(val)
+                    self.Yprev[idx] = round(self.Coef * float(val) + (1 - self.Coef) * self.Yprev[idx], 1)
                     break
                 else:
                     continue
             else:
-                self.Yprev[idx] = self.Yprev[idx]/3  # исходных данных нет
-            zbx.send(
-                zbxkey + ('[{0}]'.format(key)), val, item[4])
-            zbx.send(
-                zbxkey + ('[{0}_Yprev]'.format(key)), self.Yprev[idx], item[4])
+                if self.Yprev[idx] is not None:
+                    self.Yprev[idx] = self.Yprev[idx]/3  # исходных данных нет, уменьшаем значение
+            if self.Yprev[idx] is None or self.Yprev[idx] is not None and self.Yprev[idx] <= 0:
+                self.Yprev[idx] = None
+            else:
+                if val is not None:
+                    if item[6] == Plugin.VALUE_TYPE.numeric_float:
+                        zbx.send('{0}[{1}]'.format(zbxkey, key), float(val), delta)
+                        self.log.info('{0}[{1}] '.format(zbxkey, key)+str(val))
+                    else:
+                        zbx.send('{0}[{1}]'.format(zbxkey, key), int(val), delta)
+                        self.log.info('{0}[{1}] '.format(zbxkey, key)+str(val))
+                    zbx.send('{0}[{1}_Yprev]'.format(zbxkey, key), float(self.Yprev[idx]), delta)
+                    self.log.info('{0}[{1}_Yprev] '.format(zbxkey, key)+str(self.Yprev[idx]))
+                # debug...
+                #if key == 'calls_upd':
 
     # Declare zabbix items for template
     def items(self, template):
@@ -124,14 +109,16 @@ class Activity(Plugin):
         for idx, item in enumerate(self.Items):
             key, zbxkey = item[0], item[1]
             result += template.item({
-                'name': 'test: {0}'.format(item[2]),
+                'name': 'sparkmes: {0}'.format(item[2]),
                 'key': zbxkey + ('[{0}]'.format(key)),
-                'units': item[3]
+                'units': item[3],
+                'value_type': item[6]
             })
             result += template.item({
-                'name': 'test: {0}_Yprev'.format(item[2]),
+                'name': 'sparkmes: {0}_Yprev'.format(item[2]),
                 'key': zbxkey + ('[{0}_Yprev]'.format(key)),
-                'units': item[3]
+                'units': item[3],
+                'value_type': Plugin.VALUE_TYPE.numeric_float
             })
         return result
 
@@ -140,20 +127,22 @@ class Activity(Plugin):
         result = ''
         for name in self.Graphs:
             items = []
-            for item in name[2]:
-                zbxkey = 'pgsql.queries.test[{0}]'.format(item[0])
+            for idx, item in enumerate(name[2]):
+                zbxkey =  'pgsql.queries.sparkmes[{0}]'.format(item[0])
                 if item[3] is None:
                     items.append({
                         'key': zbxkey,
                         'color': item[1],
-                        'yaxisside': item[2]
+                        'yaxisside': item[2],
+                        'sortorder': idx
                     })
                 else:
                     items.append({
                         'key': zbxkey,
                         'color': item[1],
                         'yaxisside': item[2],
-                        'drawtype': item[3]
+                        'drawtype': item[3],
+                        'sortorder': idx
                     })
             graph = {'name': name[0], 'items': items, 'type': name[1]}
             result += template.graph(graph)
