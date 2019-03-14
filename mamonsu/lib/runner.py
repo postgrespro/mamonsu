2657c3424420f91b032cfdee4c84b9e5d62ddcce
Alexandr Popov <a.popov@postgrespro.ru>
2019-03-14 12:50:40 +0300

    fix

--- mamonsu/lib/runner.py
+++ mamonsu/lib/runner.py
@@ -66,7 +66,7 @@ def start():
                 with codecs.open(commands[2], 'w', 'utf-8') as f:
                     f.write(template.xml(plugins))
                     sys.exit(0)
-            elif commands[1] == 'template-zabbix-zgent':
+            elif commands[1] == 'template-zabbix-agent':
                 pass
             else:
                 print_total_help()
