7dcce68a8f538d126f936eba8093a25bc0430fe0
d.vilkova <d.vilkova@postgrespro.ru>
2019-04-03 19:34:14 +0300

    remove comments

--- mamonsu/lib/get_keys.py
+++ mamonsu/lib/get_keys.py
@@ -21,17 +21,14 @@ class GetKeys(object):
     def _get_all(self, keys_and_queries='keys_and_queries', plugins=[]):
         result = ''
         for plugin in plugins:
-            print("plugin.AgentPluginType",plugin.AgentPluginType)
-            print("self.plg_type",self.plg_type)
             if plugin.AgentPluginType == self.plg_type or self.plg_type =='all': # check if plugin is required for current input option
                 row = getattr(plugin, keys_and_queries)(self)  # get Items of this particular plugin
                 if row is None:
                     continue
                 if plugin.AgentPluginType == 'pg':
-                    row  = re.sub(r"\,\"", ",/opt/pgpro/std-10/bin/psql -qAt -p 5433 -U postgres -d postgres -c \"",
-                                     row )
-                print("result", row )
-                print("\n")
+                    row = re.sub(r"\,\"", ",/opt/pgpro/std-10/bin/psql -qAt -p 5433 -U postgres -d postgres -c \"", row)
+                #print("result", row )
+                #print("\n")
                 result += row
         return result
 
@@ -44,8 +41,4 @@ class GetKeys(object):
             one = re.sub(r"\[", ".", one)  # for zabbix-agent type of key representation
             # add parameters for psql to string
             result += 'UserParameter={0}\n'.format(one[2:-1])  # lose brackets
-       # result = re.sub('set\(\[', '', quiry)
-       # result = re.sub(r"[\\]", "", result)
-       # result = re.sub(r"\]\)", "", result)
-       # result = result[1:-1]
         return result
