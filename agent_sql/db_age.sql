b5a29f4cfc5de01088fd402bfa07afa338c0391a
d.vilkova <d.vilkova@postgrespro.ru>
2019-06-06 10:47:06 +0300

    Remove directory agent_sql

--- agent_sql/db_age.sql
+++
@@ -1,3 +0,0 @@
-select age(datfrozenxid) from pg_catalog.pg_database where datistemplate = false and datname = :'p1'
-
- 
--- agent_sql/db_size.sql
+++
@@ -1,1 +0,0 @@
-select pg_database_size(datname::text) from pg_catalog.pg_database where datistemplate = false and datname = :'p1';
