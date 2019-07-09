dd9fd655a390bb7ef8f71d4927554c0bed8305a1
d.vilkova <d.vilkova@postgrespro.ru>
2019-07-09 10:01:46 +0300

    test: fix test suite

--- test.py
+++ test.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python
-import os, sys                            # get unix, python services 
-from stat import ST_SIZE                  # or use os.path.getsize
-from glob import glob                     # file name expansion
-from os.path import exists                # file exists test
-from time import time, ctime              # time functions
+import os, sys  # get unix, python services
+from stat import ST_SIZE  # or use os.path.getsize
+from glob import glob  # file name expansion
+from os.path import exists  # file exists test
+from time import time, ctime  # time functions
 
-print 'RegTest start.' 
-print 'user:', os.environ['USER']         # environment variables
-print 'path:', os.getcwd(  )              # current directory
-print 'time:', ctime(time(  )), '\n'
-program = sys.argv[1]                     # two command-line args
-testdir = sys.argv[2]
+print('RegTest start.')
+print('user:', os.environ['USER'])  # environment variables
+print('path:', os.getcwd())  # current directory
+print('time:', ctime(time()), '\n')
+# program = sys.argv[1]  # two command-line args
+# testdir = sys.argv[2]
 
-for test in glob(testdir + '/*.in'):      # for all matching input files
-        f=open(test,'r')
-	line = f.readline()
-	line = line.strip()
-	os.system('%s %s 2>&1' % (program, line))
-        os.system('diff %s.out %s.out.exp > %s.diffs' % ((test,)*3) )
-        if os.stat(test + '.diffs')[ST_SIZE] == 0:
-            print 'PASSED:', test 
-            os.remove(test + '.diffs')
-        else:
-            print 'FAILED:', test, '(see %s.diffs)' % test
+for test in glob(os.getcwd() + "/regression" + '/*.in'):  # for all matching input files
+    f = open(test, 'r')
+    line = f.readline()
+    line = line.strip()
+    os.system('%s %s 2>&1' % ("./mamonsu.py", line))
+    os.system('diff %s.out %s.out.exp > %s.diffs' % ((test,) * 3))
+    if os.stat(test + '.diffs')[ST_SIZE] == 0:
+        print(
+            'PASSED:', test)
+        os.remove(test + '.diffs')
+    else:
+        print(
+            'FAILED:', test, '(see %s.diffs)' % test)
+    f.close()
 
-print 'RegTest done:', ctime(time(  ))
+print('RegTest done:', ctime(time()))
