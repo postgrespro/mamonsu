da2bba016c3b026c2e955b289de81a240ca40ee7
d.vilkova <d.vilkova@postgrespro.ru>
2019-08-09 17:04:02 +0300

    fix: add replication metric for native zabbix agent

--- mamonsu/plugins/pgsql/xlog.py
+++ mamonsu/plugins/pgsql/xlog.py
@@ -6,14 +6,14 @@ from .pool import Pooler
 
 
 class Xlog(Plugin):
-
     DEFAULT_CONFIG = {'lag_more_then_in_sec': str(60 * 5)}
     query_wal_lsn_diff = " select pg_catalog.pg_wal_lsn_diff " \
                          "(pg_catalog.pg_current_wal_lsn(), '0/00000000');"
     query_xlog_lsn_diff = "select pg_catalog.pg_xlog_location_diff " \
-                         "(pg_catalog.pg_current_xlog_location(), '0/00000000');"
+                          "(pg_catalog.pg_current_xlog_location(), '0/00000000');"
     key_wall = 'pgsql.wal.write{0}'
     key_count_wall = "pgsql.wal.count{0}"
+    key_replication = "pgsql.replication_lag{0}"
     AgentPluginType = 'pg'
 
     def run(self, zbx):
@@ -45,20 +45,20 @@ class Xlog(Plugin):
         else:
             delta = Plugin.DELTA_SPEED
         result += template.item({
-                'name': 'PostgreSQL: wal write speed',
-                'key': self.right_type(self.key_wall),
-                'units': Plugin.UNITS.bytes,
-                'delay': self.plugin_config('interval'),
-                'delta': delta
-            }) + template.item({
-                'name': 'PostgreSQL: streaming replication lag',
-                'key': 'pgsql.replication_lag[sec]',
-                'delay': self.plugin_config('interval')
-            }) + template.item({
-                'name': 'PostgreSQL: count of xlog files',
-                'key': self.right_type(self.key_count_wall),
-                'delay': self.plugin_config('interval')
-            })
+            'name': 'PostgreSQL: wal write speed',
+            'key': self.right_type(self.key_wall),
+            'units': Plugin.UNITS.bytes,
+            'delay': self.plugin_config('interval'),
+            'delta': delta
+        }) + template.item({
+            'name': 'PostgreSQL: streaming replication lag',
+            'key': self.right_type(self.key_replication, "sec"),
+            'delay': self.plugin_config('interval')
+        }) + template.item({
+            'name': 'PostgreSQL: count of xlog files',
+            'key': self.right_type(self.key_count_wall),
+            'delay': self.plugin_config('interval')
+        })
         return result
 
     def graphs(self, template):
@@ -66,35 +66,38 @@ class Xlog(Plugin):
             'name': 'PostgreSQL write-ahead log generation speed',
             'items': [
                 {'color': 'CC0000',
-                    'key': self.right_type(self.key_wall)}]})
+                 'key': self.right_type(self.key_wall)}]})
         result += template.graph({
             'name': 'PostgreSQL replication lag in second',
             'items': [
                 {'color': 'CC0000',
-                    'key': 'pgsql.replication_lag[sec]'}]})
+                 'key': self.right_type(self.key_replication, "sec")}]})
         result += template.graph({
             'name': 'PostgreSQL count of xlog files',
             'items': [
                 {'color': 'CC0000',
-                    'key': self.right_type(self.key_count_wall)}]})
+                 'key': self.right_type(self.key_count_wall)}]})
         return result
 
     def triggers(self, template):
         return template.trigger({
             'name': 'PostgreSQL streaming lag to high '
-            'on {HOSTNAME} (value={ITEM.LASTVALUE})',
-            'expression': '{#TEMPLATE:pgsql.replication_lag[sec].last'
-            '()}&gt;' + self.plugin_config('lag_more_then_in_sec')
+                    'on {HOSTNAME} (value={ITEM.LASTVALUE})',
+            'expression': '{#TEMPLATE:' + self.right_type(self.key_replication, "sec") + '.last()}&gt;' +
+            self.plugin_config('lag_more_then_in_sec')
         })
 
     def keys_and_queries(self, template_zabbix):
         result = []
         if self.VersionPG['number'] < LooseVersion('10'):
-            result.append('{0},$2 $1 -c "{1}"'.format(self.key_count_wall.format('[*]'), Pooler.SQL['count_xlog_files'][0]))
+            result.append(
+                '{0},$2 $1 -c "{1}"'.format(self.key_count_wall.format('[*]'), Pooler.SQL['count_xlog_files'][0]))
             result.append('{0},$2 $1 -c "{1}"'.format(self.key_wall.format('[*]'), self.query_xlog_lsn_diff))
         else:
-            result.append('{0},$2 $1 -c "{1}"'.format(self.key_count_wall.format('[*]'), Pooler.SQL['count_wal_files'][0]))
+            result.append(
+                '{0},$2 $1 -c "{1}"'.format(self.key_count_wall.format('[*]'), Pooler.SQL['count_wal_files'][0]))
             result.append('{0},$2 $1 -c "{1}"'.format(self.key_wall.format('[*]'), self.query_wal_lsn_diff))
         # FIXME for lag
+        result.append(
+            '{0},$2 $1 -c "{1}"'.format("pgsql.replication_lag.sec[*]", Pooler.SQL['replication_lag_slave_query']))
         return template_zabbix.key_and_query(result)
-
