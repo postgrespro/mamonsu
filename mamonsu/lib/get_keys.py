d83141b3f015e9abf54b245bcaecfbeabad82cbe
d.vilkova <d.vilkova@postgrespro.ru>
2019-08-16 13:57:49 +0300

    feat: add log message if some plugin does not have keys for zabbix-agent conf file

--- mamonsu/lib/get_keys.py
+++ mamonsu/lib/get_keys.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-import re
+import logging
 
 
 class GetKeys(object):
-
     plg_type = 'all'
 
     def txt(self, type, plugins=[]):
@@ -11,18 +10,22 @@ class GetKeys(object):
         self.plg_type = type
         plugins.sort(key=lambda x: x.__class__.__name__)
         # create template
-        template_data = self._get_all('keys_and_queries', plugins)   # get data from all plugins
+        template_data = self._get_all('keys_and_queries', plugins)  # get data from all plugins
         return template_data
 
     def _get_all(self, keys_and_queries, plugins=[]):
         result = ''
+        # don't need keys for zabbix agent from these classes
+        non_keys_classes = ('AgentApi', 'LogSender', 'ZbxSender')
         for plugin in plugins:
-            if plugin.AgentPluginType == self.plg_type or self.plg_type == 'all':
+            if plugin.__class__.__name__ not in non_keys_classes:
                 # check if THIS plugin is required for current input option
-                row = getattr(plugin, keys_and_queries)(self)  # get keys_and_queries of this particular plugin
-                if row is None:
-                    continue
-                result += row
+                if plugin.AgentPluginType == self.plg_type or self.plg_type == 'all':
+                    row = getattr(plugin, keys_and_queries)(self)  # get keys_and_queries of this particular plugin
+                    if row is None:
+                        logging.info('No keys for plugin {0}'.format(plugin.__class__.__name__))
+                        continue
+                    result += row
         return result
 
     def key_and_query(self, args=[]):
@@ -30,4 +33,3 @@ class GetKeys(object):
         for one in args:
             result += 'UserParameter={0}\n'.format(one)
         return result
-
