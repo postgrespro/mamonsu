8a435284b57f0f062fa61e1a9c48dc960838f067
alexander popov <a.popov@postgrespro.ru>
2020-06-17 16:27:13 +0300

    feat: add new plugin to calculate relations sizes

--- mamonsu/plugins/pgsql/__init__.py
+++ mamonsu/plugins/pgsql/__init__.py
@@ -4,5 +4,6 @@ __all__ += ['pg_stat_statement', 'pg_buffercache', 'pg_wait_sampling']
 __all__ += ['checkpoint', 'oldest', 'pg_locks']
 __all__ += ['cfs']
 __all__ += ['archive_command']
+__all__ += ['relations_size']
 
 from . import *
