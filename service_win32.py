57b3b1817df10030eb35e69b86ae22cd4a80b758
Victor Spirin <v.spirin@postgrespro.ru>
2020-08-26 01:23:11 +0300

    BUILD: changed service for windows , PGPRO-4107

--- service_win32.py
+++ service_win32.py
@@ -2,12 +2,35 @@ import win32serviceutil
 import win32service
 import win32event
 import win32evtlogutil
-#import servicemanager
+import servicemanager
 import os
+import sys
+
+# import socket
+# import time
+import logging
+
+# import socket
+# import os
+# import sys
+# import glob
+# import time
+# import traceback
+# import json
+# import threading
+# import signal
+# import codecs
+# import re
 
 from threading import Thread
 from mamonsu.lib.config import Config
 from mamonsu.lib.supervisor import Supervisor
+
+# logging.basicConfig(
+    # filename = 'c:\\Temp\\hello-service.log',
+    # level = logging.DEBUG, 
+    # format = '[helloworld-service] %(levelname)-7.7s %(message)s'
+# )
 
 class MamonsuSvc(win32serviceutil.ServiceFramework):
 
@@ -26,21 +49,31 @@ class MamonsuSvc(win32serviceutil.ServiceFramework):
 
     def SvcDoRun(self):
 
-        # __file__ == 'service_win32.py'
-        exe_dir = os.path.dirname(os.path.dirname(__file__))
-        os.chdir(exe_dir)
+        
+      
+        # determine if application is a script file or frozen exe       
+        if getattr(sys, 'frozen', False):
+            exe_dir = os.path.dirname(sys.executable)
+        elif __file__:
+            # exe_dir = C:\WINDOWS\system32 for service
+            exe_dir = os.path.dirname(os.path.abspath(__file__))
 
-        # win32evtlogutil.ReportEvent(
-        #     self._svc_name_,
-        #     servicemanager.PYS_SERVICE_STARTED,
-        #     0,
-        #     servicemanager.EVENTLOG_INFORMATION_TYPE,
-        #     (self._svc_name_, ''))
+        logging.info("exe_dir="+exe_dir)
 
-        config_file = os.path.join(exe_dir, 'agent_win32.conf')
-#        config = Config(config_file)
-        config = Config('c:\\mamonsu\\agent_win32.conf')
+        win32evtlogutil.ReportEvent(
+            self._svc_name_,
+            servicemanager.PYS_SERVICE_STARTED,
+            0,
+            servicemanager.EVENTLOG_INFORMATION_TYPE,
+            (self._svc_name_, ''))
 
+        # logging.basicConfig( filename = os.path.join(exe_dir, 'agent.log'), level = logging.DEBUG,  format = '[helloworld-service] %(levelname)-7.7s %(message)s' )
+        
+        # config_file = os.path.join(exe_dir, 'agent_win32.conf')
+        config_file = os.path.join(exe_dir, 'agent.conf')
+        logging.info(config_file)
+        config = Config(config_file)
+        # config = Config('c:\\mamonsu\\agent_win32.conf')
         supervisor = Supervisor(config)
         # win32evtlogutil.ReportEvent(
         #     self._svc_name_,
@@ -52,19 +85,26 @@ class MamonsuSvc(win32serviceutil.ServiceFramework):
         thread = Thread(target=supervisor.start)
         thread.daemon = True
         thread.start()
-
         while True:
             rc = win32event.WaitForSingleObject(
                 self.hWaitStop, win32event.INFINITE)
             if rc == win32event.WAIT_OBJECT_0:
-                # win32evtlogutil.ReportEvent(
-                #     self._svc_name_,
-                #     servicemanager.PYS_SERVICE_STOPPED,
-                #     0,
-                #     servicemanager.EVENTLOG_INFORMATION_TYPE,
-                #     (self._svc_name_, ''))
+                win32evtlogutil.ReportEvent(
+                    self._svc_name_,
+                    servicemanager.PYS_SERVICE_STOPPED,
+                    0,
+                    servicemanager.EVENTLOG_INFORMATION_TYPE,
+                    (self._svc_name_, ''))
                 break
 
 
+#if __name__ == '__main__':
+#    win32serviceutil.HandleCommandLine(MamonsuSvc)
+
 if __name__ == '__main__':
-    win32serviceutil.HandleCommandLine(MamonsuSvc)
+    if len(sys.argv) == 1:
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(MamonsuSvc)
+        servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(MamonsuSvc)
