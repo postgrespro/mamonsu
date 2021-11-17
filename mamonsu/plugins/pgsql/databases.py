14edd05788164e0734bfe74c7ebe87930762767f
cuprumtan <cuprumtan@gmail.com>
2021-11-17 11:50:17 +0300

    feature: added new invalid indexes metrics

--- mamonsu/plugins/pgsql/databases.py
+++ mamonsu/plugins/pgsql/databases.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from distutils.version import LooseVersion
 from mamonsu.plugins.pgsql.plugin import PgsqlPlugin as Plugin
 from .pool import Pooler
 from mamonsu.lib.zbx_template import ZbxTemplate
@@ -16,6 +17,11 @@ class Databases(Plugin):
                  " datistemplate = false and datname = :'p1';"
     query_age = "select age(datfrozenxid) from pg_catalog.pg_database where datistemplate = false " \
                 "and datname = :'p1';"
+    query_invalid_indexes = "SELECT count(*) " \
+                            "FROM pg_catalog.pg_class, pg_catalog.pg_index " \
+                            "WHERE pg_catalog.pg_index.indisvalid = false " \
+                            "AND pg_catalog.pg_index.indexrelid = pg_catalog.pg_class.oid " \
+                            "AND substring(pg_catalog.pg_class.relname, '([^_]+$)') ~ '^cc{0}';"
 
     # queries for zabbix agent
     query_agent_discovery = "SELECT json_build_object ('data',json_agg(json_build_object('{#DATABASE}',d.datname)))" \
@@ -26,10 +32,14 @@ class Databases(Plugin):
     key_db_age = "pgsql.database.max_age{0}"
     key_db_bloating_tables = "pgsql.database.bloating_tables{0}"
     key_autovacumm = "pgsql.autovacumm.count{0}"
+    key_invalid_rebuilt_indexes = "pgsql.database.invalid_rebuilt_indexes{0}"
+    key_invalid_old_indexes = "pgsql.database.invalid_old_indexes{0}"
 
     DEFAULT_CONFIG = {'min_rows': str(50), 'bloat_scale': str(0.2)}
 
     def run(self, zbx):
+        if Pooler.server_version_greater('12'):
+            self.query_invalid_indexes = self.query_invalid_indexes[:-1] + " AND pg_catalog.pg_class.oid NOT IN (SELECT index_relid FROM pg_catalog.pg_stat_progress_create_index);"
         result = Pooler.query('select \
             datname, pg_database_size(datname::text), age(datfrozenxid) \
             from pg_catalog.pg_database where datistemplate = false')
@@ -49,6 +59,18 @@ class Databases(Plugin):
             zbx.send(
                 'pgsql.database.bloating_tables[{0}]'.format(info[0]),
                 int(bloat_count))
+            invalid_rebuilt_indexes_count = Pooler.query(
+                self.query_invalid_indexes.format('new'),
+                info[0])[0][0]
+            invalid_old_indexes_count = Pooler.query(
+                self.query_invalid_indexes.format('old'),
+                info[0])[0][0]
+            zbx.send(
+                'pgsql.database.invalid_rebuilt_indexes[{0}]'.format(info[0]),
+                int(invalid_rebuilt_indexes_count))
+            zbx.send(
+                'pgsql.database.invalid_old_indexes[{0}]'.format(info[0]),
+                int(invalid_old_indexes_count))
         zbx.send('pgsql.database.discovery[]', zbx.json({'data': dbs}))
         del dbs, bloat_count
 
@@ -100,6 +122,12 @@ class Databases(Plugin):
              'delay': self.plugin_config('interval')},
             {'key': self.right_type(self.key_db_bloating_tables, var_discovery="{#DATABASE},"),
              'name': 'Count of bloating tables in database: {#DATABASE}',
+             'delay': self.plugin_config('interval')},
+            {'key': self.right_type(self.key_invalid_rebuilt_indexes, var_discovery="{#DATABASE},"),
+             'name': 'Count of indexes corrupted during REINDEX in database: {#DATABASE}',
+             'delay': self.plugin_config('interval')},
+            {'key': self.right_type(self.key_invalid_old_indexes, var_discovery="{#DATABASE},"),
+             'name': 'Count of old invalid indexes in database: {#DATABASE}',
              'delay': self.plugin_config('interval')}
         ]
         graphs = [
@@ -129,9 +157,20 @@ class Databases(Plugin):
                      'yaxisside': 1}]
             }
         ]
-        return template.discovery_rule(rule=rule, conditions=conditions, items=items, graphs=graphs)
+        triggers = [{
+            'name': 'PostgreSQL corrupted indexes during REINDEX in database '
+                    '{#DATABASE} (hostname={HOSTNAME} value={ITEM.LASTVALUE})',
+            'expression': '{#TEMPLATE:pgsql.database.invalid_rebuilt_indexes[{#DATABASE}].last()}&gt;0'},
+            {
+                'name': 'PostgreSQL old invalid indexes in database '
+                        '{#DATABASE} (hostname={HOSTNAME} value={ITEM.LASTVALUE})',
+                'expression': '{#TEMPLATE:pgsql.database.invalid_old_indexes[{#DATABASE}].last()}&gt;0'}
+        ]
+        return template.discovery_rule(rule=rule, conditions=conditions, items=items, graphs=graphs, triggers=triggers)
 
     def keys_and_queries(self, template_zabbix):
+        if LooseVersion(self.VersionPG) >= LooseVersion('12'):
+            self.query_invalid_indexes = self.query_invalid_indexes[:-1] + " AND pg_catalog.pg_class.oid NOT IN (SELECT index_relid FROM pg_catalog.pg_stat_progress_create_index);"
         result = ['{0},$2 $1 -c "{1}"'.format(self.key_autovacumm.format("[*]"), Pooler.SQL['count_autovacuum'][0]),
                   '{0},$2 $1 -c "{1}"'.format(self.key_db_discovery.format("[*]"), self.query_agent_discovery),
                   '{0},echo "{1}" | $3 $2 -v p1="$1"'.format(self.key_db_size.format("[*]"), self.query_size),
@@ -139,5 +178,9 @@ class Databases(Plugin):
                   '{0},$3 $2 -d "$1" -c "{1}"'.format(self.key_db_bloating_tables.format("[*]"),
                                                       self.query_bloating_tables.format(
                                                           self.plugin_config('bloat_scale'),
-                                                          self.plugin_config('min_rows')))]
+                                                          self.plugin_config('min_rows'))),
+                  '{0},$3 $2 -d "$1" -c "{1}"'.format(self.key_invalid_rebuilt_indexes.format("[*]"),
+                                                      self.query_invalid_indexes.format('new')),
+                  '{0},$3 $2 -d "$1" -c "{1}"'.format(self.key_invalid_old_indexes.format("[*]"),
+                                                      self.query_invalid_indexes.format('old'))]
         return template_zabbix.key_and_query(result)
