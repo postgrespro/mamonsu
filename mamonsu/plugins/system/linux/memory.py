0f87624c63e6bbc6f928ccea860411450773582a
cuprumtan <cuprumtan@gmail.com>
2022-04-21 15:39:40 +0300

    feature: added new System Free/Used Memory graph - added new system metric 'Total Memory'; - added new simple graph showing amount of all system memory and a group of used, free and cached memory;

--- mamonsu/plugins/system/linux/memory.py
+++ mamonsu/plugins/system/linux/memory.py
@@ -5,7 +5,7 @@ from mamonsu.lib.zbx_template import ZbxTemplate
 class Memory(Plugin):
     AgentPluginType = 'sys'
     query_agent = "cat /proc/meminfo | awk '/^{0}\:/ "
-    query_agent_apps = "MemTotal=$(cat /proc/meminfo | awk '/MemTotal\:/ { print $2 }'); " \
+    query_agent_used = "MemTotal=$(cat /proc/meminfo | awk '/MemTotal\:/ { print $2 }'); " \
                        "SUM=$(cat /proc/meminfo | awk '/(MemFree|Buffers|(Swap)?Cached|Slab|PageTables)\:/ " \
                        "{ SUM += $2 } END {print SUM}'); echo $((($MemTotal-$SUM)*1024))"
     query_agent_swap = "expr `grep -Ei 'Swap(Total|Free)' /proc/meminfo | awk '{print $2 * 1024}' | paste -s -d '-' " \
@@ -17,48 +17,22 @@ class Memory(Plugin):
     # 2. virtual memory
 
     Items = [
-        # zbx_key, meminfo_key, name, color
-        ('apps', None,
-         'Apps: User-space applications', 'CC0000'),
-
-        ('buffers', 'Buffers',
-         'Buffers: Block device cache and dirty', '00CC00'),
-
-        ('swap', None,
-         'Swap: Swap space used', '0000CC'),
-
-        ('cached', 'Cached',
-         'Cached: Parked file data (file content) cache', 'CC00CC'),
-
-        ('unused', 'MemFree',
-         'Free: Wasted memory', '000000'),
-
-        ('available', 'MemAvailable',
-         'Free: Available', '008000'),
-
-        ('slab', 'Slab',
-         'Slab: Kernel used memory (inode cache)', 'CCCC00'),
-
-        ('swap_cache', 'SwapCached',
-         'SwapCached: Fetched unmod yet swap pages', '777777'),
-
-        ('page_tables', 'PageTables',
-         'PageTables: Map bt virtual and physical', '770000'),
-
-        ('vmalloc_used', 'VmallocUsed',
-         'VMallocUsed: vmaloc() allocated by kernel', '000077'),
-
-        ('committed', 'Committed_AS',
-         'Committed_AS: Total committed memory', '007700'),
-
-        ('mapped', 'Mapped',
-         'Mapped: All mmap()ed pages', 'DF0000'),
-
-        ('active', 'Active',
-         'Active: Memory recently used', '00DF00'),
-
-        ('inactive', 'Inactive',
-         'Inactive: Memory not currently used', '0000DF')
+        # zbx_key, meminfo_key, name, color, drawtype
+        ('total', 'MemTotal', 'Memory Total', '5CA8FF', 4),
+        ('used', None, 'Used: User-space applications', 'CC0000', 1),
+        ('buffers', 'Buffers', 'Buffers: Block device cache and dirty', '00CC00', 1),
+        ('swap', None, 'Swap: Swap space used', '0000CC', 1),
+        ('cached', 'Cached', 'Cached: Parked file data (file content) cache', 'CC00CC', 1),
+        ('unused', 'MemFree', 'Free: Wasted memory', '000000', 1),
+        ('available', 'MemAvailable', 'Free: Available', '008000', 1),
+        ('slab', 'Slab', 'Slab: Kernel used memory (inode cache)', 'CCCC00', 1),
+        ('swap_cache', 'SwapCached', 'SwapCached: Fetched unmod yet swap pages', '777777', 1),
+        ('page_tables', 'PageTables', 'PageTables: Map bt virtual and physical', '770000', 1),
+        ('vmalloc_used', 'VmallocUsed', 'VMallocUsed: vmaloc() allocated by kernel', '000077', 1),
+        ('committed', 'Committed_AS', 'Committed_AS: Total committed memory', '007700', 1),
+        ('mapped', 'Mapped', 'Mapped: All mmap()ed pages', 'DF0000', 1),
+        ('active', 'Active', 'Active: Memory recently used', '00DF00', 1),
+        ('inactive', 'Inactive', 'Inactive: Memory not currently used', '0000DF', 1)
     ]
 
     def run(self, zbx):
@@ -76,7 +50,7 @@ class Memory(Plugin):
             zbx_key, meminfo_key = item[0], item[1]
             if meminfo_key is not None:
                 result[zbx_key] = meminfo.get(meminfo_key) or 0
-        result['apps'] = meminfo['MemTotal'] - result['unused'] \
+        result['used'] = meminfo['MemTotal'] - result['unused'] \
                          - result['buffers'] - result['cached'] - result['slab'] \
                          - result['page_tables'] - result['swap_cache']
         result['swap'] = (meminfo.get('SwapTotal') or 0) \
@@ -103,19 +77,35 @@ class Memory(Plugin):
             return []
 
     def graphs(self, template, dashboard=False):
-        items = []
+        result = ""
+        all_items = []
+        free_used_items = []
         for item in self.Items:
-            items.append({
+            all_items.append({
                 'key': self.right_type(self.key, item[0]),
                 'color': item[3]
             })
-        graph = {
-            'name': 'Memory overview', 'height': 400,
-            'type': 1, 'items': items}
+            if item[0] in ['cached', 'available', 'used', 'total']:
+                free_used_items.append({
+                    'key': self.right_type(self.key, item[0]),
+                    'color': item[3],
+                    'drawtype': item[4]
+                })
+        graphs = [{'name': 'Server Memory Detailed Overview',
+                   'height': 400,
+                   'type': 1,
+                   'items': all_items},
+                  {'name': 'Server Free/Used Memory Overview',
+                   'height': 400,
+                   'type': 0,
+                   'items': free_used_items}
+                  ]
         if not dashboard:
-            return template.graph(graph)
+            for graph in graphs:
+                result += template.graph(graph)
+            return result
         else:
-            return [{'dashboard': {'name': 'Memory overview',
+            return [{'dashboard': {'name': 'Server Memory Detailed Overview',
                                    'page': ZbxTemplate.dashboard_page_overview['name'],
                                    'size': ZbxTemplate.dashboard_widget_size_medium,
                                    'position': 4}}]
@@ -123,9 +113,9 @@ class Memory(Plugin):
     def keys_and_queries(self, template_zabbix):
         result = []
         for item in self.Items:
-            if item[1] is None and item[0] == 'apps':
+            if item[1] is None and item[0] == 'used':
                 result.append('{0},{1}'.format(self.key.format('.' + item[0]),
-                                               self.query_agent_apps))
+                                               self.query_agent_used))
             elif item[1] is None and item[0] == 'swap':
                 result.append('{0},{1}'.format(self.key.format('.' + item[0]),
                                                self.query_agent_swap))
