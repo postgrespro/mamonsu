2ca28ccf39503df7a5ee9c30978b9e8c31f10608
cuprumtan <cuprumtan@gmail.com>
2022-05-18 09:21:49 +0300

    refactoring: fixed spelling

--- mamonsu/plugins/system/linux/proc_stat.py
+++ mamonsu/plugins/system/linux/proc_stat.py
@@ -142,7 +142,7 @@ class ProcStat(Plugin):
     def triggers(self, template, dashboard=False):
         return template.trigger(
             {
-                "name": "Process fork-rate to frequently on {HOSTNAME}",
+                "name": "Process fork-rate too frequently on {HOSTNAME}",
                 "expression": "{#TEMPLATE:" + self.right_type("system.processes{0}", "forkrate") +
                               ".min(5m)}&gt;" + str(self.ForkRate)
             }
