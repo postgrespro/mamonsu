3419758739872509632590cbd866e3f3257b6c91
Maxim Styushin <makcimkos@gmail.com>
2024-07-09 14:43:50 +0400

    cicd:  fix centos7 repo urls

--- github-actions-tests/tests.sh
+++ github-actions-tests/tests.sh
@@ -36,6 +36,32 @@ REPO="sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms
 if [ "${OS}" = "centos:7" ]; then
     # install and set up components missing in docker image (sudo, wget, bc, unzip)
     yum clean all
+    cat << REPO > /etc/yum.repos.d/CentOS-Base.repo
+[base]
+name=CentOS-\$releasever - Base
+baseurl=http://vault.centos.org/7.9.2009/os/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[updates]
+name=CentOS-\$releasever - Updates
+baseurl=http://vault.centos.org/7.9.2009/updates/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[extras]
+name=CentOS-\$releasever - Extras
+baseurl=http://vault.centos.org/7.9.2009/extras/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[centosplus]
+name=CentOS-\$releasever - Plus
+baseurl=http://vault.centos.org/7.9.2009/centosplus/\$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+REPO
     yum update -y
     yum install -y sudo
     PACKAGE_MANAGER_INSTALL="sudo yum -y install"
